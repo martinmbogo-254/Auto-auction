efe3259ba81aae455daef7db79cf10981e95c941
unknown <mbogomartin25@gmail.com>
2024-07-04 05:05:52 +0300

    initial db models

--- auctions/vehicles/admin.py
+++ auctions/vehicles/admin.py
@@ -1,6 +1,56 @@
 from django.contrib import admin
-from .admin import Vehicle
-# Register your models here.
-admin.site.Register (Vehicle)
+from .models import (
+    VehicleImage, VehicleMake, VehicleModel, 
+    ManufactureYear, FuelType, VehicleBody, 
+    User, Vehicle, Bid
+)
 
-    
+class VehicleImageInline(admin.TabularInline):
+    model = VehicleImage
+    extra = 1  # Number of empty forms to display
+
+class BidInline(admin.TabularInline):
+    model = Bid
+    extra = 1  # Number of empty forms to display
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'make', 'model', 'YOM', 'mileage', 'engine_cc', 'body_type', 'fuel_type', 'created_by', 'get_bid_status_display', 'reserve_price', 'created_at', 'updated_at')
+    search_fields = ('make__name', 'model__name', 'YOM__year', 'created_by__email', 'bid_status')
+    list_filter = ('make', 'model', 'YOM', 'body_type', 'fuel_type', 'created_at', 'updated_at')
+    inlines = [VehicleImageInline, BidInline]
+
+@admin.register(VehicleMake)
+class VehicleMakeAdmin(admin.ModelAdmin):
+    list_display = ( 'name',)
+    search_fields = ('name',)
+
+@admin.register(VehicleModel)
+class VehicleModelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(ManufactureYear)
+class ManufactureYearAdmin(admin.ModelAdmin):
+    list_display = ('year',)
+    search_fields = ('year',)
+
+@admin.register(FuelType)
+class FuelTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    class Meta:
+        verbose_name_plural = "Fuel Types"
+
+@admin.register(VehicleBody)
+class VehicleBodyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone_number', 'id_number', 'name', 'is_admin', 'created_at')
+    search_fields = ('email', 'phone_number', 'id_number', 'name')
+    list_filter = ('is_admin', 'created_at')
+
+
--- auctions/vehicles/models.py
+++ auctions/vehicles/models.py
@@ -1,4 +1,80 @@
 from django.db import models
 
+
+
+class VehicleMake(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Makes"
+    def __str__(self):
+            return self.name
+
+class VehicleModel(models.Model):
+    name = models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = "Models"
+    def __str__(self):
+            return self.name
+
+class ManufactureYear(models.Model):
+    year = models.IntegerField()
+    class Meta:
+        verbose_name_plural = "Years of Manufacture"
+    def __str__(self):
+            return str(self.year)   
+
+class FuelType(models.Model):
+    name = models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = "Fuel Types"
+    def __str__(self):
+            return self.name
+
+class VehicleBody(models.Model):
+    name = models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = " Vehicle Bodies"
+    def __str__(self):
+            return self.name
+
+class User(models.Model):
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    id_number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255)
+    is_admin = models.BooleanField(default=False)
+
 class Vehicle(models.Model):
+    BID_STATUS_CHOICES = [
+        ('open', 'Open'),
+        ('closed', 'Closed'),
+        ('pending', 'Pending'),
+        ('won', 'Won'),
+        ('lost', 'Lost'),
+    ]
     make = models.ForeignKey(VehicleMake, on_delete=models.CASCADE)
+    model = models.ForeignKey(VehicleModel, on_delete=models.CASCADE)
+    YOM = models.ForeignKey(ManufactureYear, on_delete=models.CASCADE)
+    mileage = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    engine_cc = models.IntegerField()
+    body_type = models.ForeignKey(VehicleBody, on_delete=models.CASCADE)
+    fuel_type = models.ForeignKey(FuelType, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_status = models.CharField(max_length=10, choices=BID_STATUS_CHOICES, default='open')
+    reserve_price = models.IntegerField()
+
+class VehicleImage(models.Model):
+    vehicle= models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    image = models.CharField(max_length=255)
+    class Meta:
+        verbose_name_plural = "Images"
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
