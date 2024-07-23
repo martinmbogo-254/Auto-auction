f47c872d9a906e8ee1a482e7041e738bd8e6064b
unknown <mbogomartin25@gmail.com>
2024-07-23 11:01:45 +0300

    added changes to the filter functionality

--- auctions/vehicles/filters.py
+++ auctions/vehicles/filters.py
@@ -8,4 +8,9 @@ class VehicleFilter(django_filters.FilterSet):
     # name=CharFilter(field_name='name', lookup_expr='icontains')
     class Meta:
         model = Vehicle
-        fields = {'make','model','YOM','body_type'}
+        fields = {'make','model','YOM','body_type','engine_cc','fuel_type'}
+
+    def __init__(self, *args, **kwargs):
+        super(VehicleFilter, self).__init__(*args, **kwargs)
+        for field_name, field in self.filters.items():
+            field.field.widget.attrs.update({'class': 'form-control'})
