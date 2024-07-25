eb28bc37c97c388d06d3e68198e5003b6ff18217
unknown <mbogomartin25@gmail.com>
2024-07-25 16:26:13 +0300

    added comma separation to amount field on my bid form

--- auctions/vehicles/forms.py
+++ auctions/vehicles/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Bidding
+from .fields import CommaSeparatedIntegerField
+from .widgets import CommaSeparatedIntegerWidget
 
 class BidForm(forms.ModelForm):
+    amount = CommaSeparatedIntegerField(widget=CommaSeparatedIntegerWidget(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Bidding
-        fields = ['amount',]
+        fields = ['amount']
