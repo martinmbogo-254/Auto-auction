e6d6585c16e99c569613a8f20ba91e081d7f1d54
Martin <mbogomartin25@gmail.com>
2024-10-29 17:37:19 +0300

    added storage yard links

--- vehicles/models.py
+++ vehicles/models.py
@@ -51,6 +51,8 @@ class Yard(models.Model):
     name = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
 
+    def __str__(self):
+            return self.name
 class Financier(models.Model):
     name = models.CharField(max_length=255)
 class Vehicle(models.Model):
