a5f57187adba9e7a8d2039817480f52df948acc9
Martin <mbogomartin25@gmail.com>
2025-01-10 16:20:08 +0300

    updates

--- vehicles/views.py
+++ vehicles/views.py
@@ -26,7 +26,7 @@ def reports(request):
 # Create your views here.
 def homepage(request):
     upcoming_auctions = Vehicle.objects.filter(status='available')
-    hotsale = Vehicle.objects.filter(is_hotsale =True).first()
+    hotsale = Vehicle.objects.filter(is_hotsale =True)
     context = {
         'upcoming_auctions': upcoming_auctions,
         'hotsale':hotsale
