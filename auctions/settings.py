04a9e88ec2a68fcd3703ad5a1419ed8516a84475
unknown <mbogomartin25@gmail.com>
2025-01-21 18:31:01 +0300

    set email from/sender to be from support@riverlong.com

--- auctions/settings.py
+++ auctions/settings.py
@@ -164,7 +164,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
